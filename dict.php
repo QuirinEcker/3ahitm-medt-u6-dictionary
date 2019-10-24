af93af6f61f28baa1eae974c2385590d00da7024
Quirin <quirin.ecker@bajupa.com>
2019-10-24 08:51:33 +0200

    finished exercise

--- dict.php
+++ dict.php
@@ -1,38 +1,22 @@
 <?php
-// read and save the POST variable
-
-
 $search = $_POST['val'];
 $file = fopen("words_alpha.txt", "r") or die("Unable to open file!");
 $matches = [];
-
+$words = [];
 
 while(!feof($file)) {
     $currentWord = fgets($file);
 
-    if (stripos($currentWord, $search)) {
-        $matches[] = $currentWord . '\n';
+    if (stripos($currentWord, $search) !== false) {
+        $matches[] = $currentWord;
     }
 }
 
-echo var_dump($matches);
-// initiate new empty array $matches
-
-
-// iterate through each line of the file
-// visit https://www.w3schools.com/php/php_file_open.asp
-
-// loop here
-	// inside the loop: use stripos(str1, str2) to look for matches
-	// push matches to array $matches
-
-
 fclose($file);
 
-
-// check if nothing found -> error message
-
-
-// return result as JSON
-
-#echo json_encode($matches);
+if (sizeof($matches) === 0) {
+    $error = ["word not found"];
+    echo json_encode($error);
+} else {
+    echo json_encode($matches);
+}
--- scripts/dict.js
+++ scripts/dict.js
@@ -1,20 +1,26 @@
 window.addEventListener('load', () => {
     let submitButton = document.querySelector('#submit-button');
+    let inputField = document.querySelector('#input')
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {}
+    xhttp.open("POST", "dict.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("val=" + "load");
 
     submitButton.addEventListener('click',() => {
         let search_val = document.getElementById("input").value;
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState === 4 && this.status == 200) {
                 // parse response into JSON object
-                console.log(this.responseText)
                 let answer = JSON.parse(this.responseText);
 
                 // generate html output
                 let answer_html = "";
                 for (let value of answer) {
                     // create html code and save it in answer_html
-                    console.log(value); // test purpose
+                    answer_html += `<p> ${value} </p>`;
                 }
 
                 // send output to DOM
